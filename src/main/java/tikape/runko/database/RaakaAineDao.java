b044f53f4cc711b6623fdcb03fdaa87519e5535f
Koivu Karoliina E <kckoivu@da5-cs-b221-04.pc.helsinki.fi>
2018-02-14 15:02:38 +0200

    init

--- src/main/java/tikape/runko/database/RaakaAineDao.java
+++ src/main/java/tikape/runko/database/RaakaAineDao.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tikape.runko.database;
 
-/**
- *
- * @author kckoivu
- */
-public class RaakaAineDao {
+import java.sql.SQLException;
+import java.util.List;
+import tikape.runko.domain.RaakaAine;
+
+
+public class RaakaAineDao implements Dao<RaakaAine, Integer> {
+
+    private Database database;
+    
+    public RaakaAineDao(Database database) {
+        
+        
+    }
+    @Override
+    public RaakaAine findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<RaakaAine> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
