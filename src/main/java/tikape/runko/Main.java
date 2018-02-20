4eda631d63ac56cf1a8092929965e98d9bc0e9c4
Leppäkoski Tony A <lexlex@da5-cs-bk107-37.pc.helsinki.fi>
2018-02-20 14:21:31 +0200

    etusivulle drinkit

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -40,7 +40,7 @@ public class Main {
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
+            map.put("annokset", annosDao.findAll());
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
