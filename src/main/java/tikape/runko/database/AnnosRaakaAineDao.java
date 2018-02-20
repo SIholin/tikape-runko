dfc23da83a0e99405e843fe9d82ad0e635375e5e
Leppäkoski Tony A <lexlex@da5-cs-bk107-37.pc.helsinki.fi>
2018-02-20 13:29:27 +0200

    pois tuplametodit

--- src/main/java/tikape/runko/database/AnnosRaakaAineDao.java
+++ src/main/java/tikape/runko/database/AnnosRaakaAineDao.java
@@ -74,9 +74,4 @@ public class AnnosRaakaAineDao implements Dao<AnnosRaakaAine, Integer> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    @Override 
-    public AnnosRaakaAine saveOrUpdate(AnnosRaakaAine object) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
 }
